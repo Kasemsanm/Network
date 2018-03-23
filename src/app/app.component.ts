60826550ece25f4809579073756085cb714a7aac
kasemsanm <kasemsanm58@email.nu.ac.th>
2018-03-23 16:16:56 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 // import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { StoragePage } from '../pages/storage/storage';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   //rootPage:any = HomePage;
-  rootPage:any = LoginPage
+  rootPage:any = StoragePage
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
